5a45a1f28ce6a10c4d14ef2f3996210d29c87935
NeeloByron <99469633+NeeloByron@users.noreply.github.com>
2026-06-02 22:21:14 +0200

    declared elements

--- Client-Side Validation.js
+++ Client-Side Validation.js
@@ -2,5 +2,19 @@
 document.getElementById("SignupForm").addEventListener('submit', function(event) {
     event.preventDefault();
 
+//All Elements
+const Username = document.getElementById('Username');
+const Email = document.getElementById('Email');
+const Password = document.getElementById('Password');
+const Checkbox = document.getElementById('Checkbox');
+
+//Error span Elements
+const UsernameError = document.getElementById('UsernameError');
+const EmailError = document.getElementById('emailError');
+const PasswordError = document.getElementById('PasswordError');
+const termsError = document.getElementById('termsError');
+
+//Perform check conditions:
+//Evaluate empty inputs.
 
 })
