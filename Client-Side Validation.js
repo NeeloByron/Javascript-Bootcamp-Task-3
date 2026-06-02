4d222aaf8f120d80c147c2200506f292eedec654
NeeloByron <99469633+NeeloByron@users.noreply.github.com>
2026-06-02 21:40:07 +0200

    Capture form submissions and alt automatic tab reload

---
+++ Client-Side Validation.js
@@ -0,0 +1,5 @@
+//Capture form submissions and alt automatic tab reload
+const SignupForm = document.getElementById("SignupForm");
+SignupForm.addEventListener('submit', function(event) {
+    event.preventDefault();
+});
