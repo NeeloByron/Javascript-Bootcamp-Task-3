ad9bb4a7906d3677cff1008dc741f729217239c8
NeeloByron <99469633+NeeloByron@users.noreply.github.com>
2026-06-02 21:46:55 +0200

    Few updates on my form submission

--- Client-Side Validation.js
+++ Client-Side Validation.js
@@ -1,5 +1,6 @@
 //Capture form submissions and alt automatic tab reload
-const SignupForm = document.getElementById("SignupForm");
-SignupForm.addEventListener('submit', function(event) {
+document.getElementById("SignupForm").addEventListener('submit', function(event) {
     event.preventDefault();
-});
+
+
+})
